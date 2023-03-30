db732083ded9a129d93ce3b817bdd8ce9fdb66da
Anagnostakis Ioannis <412135+rizitis@users.noreply.github.com>
2023-03-30 19:56:01 +0300

    Delete script.sh

--- script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-line='if [ -f $CWD\/$PRGNAM.dep ]; then cp $CWD\/$PRGNAM.dep  $OUTPUT\/$PRGNAM-$VERSION-$ARCH-$BUILD$TAG.dep; fi'
-pattern='cd $PKG'
-#find . -name "*.SlackBuild" -exec sed -i '/$pattern/d' {} \;
-find . -name "*.SlackBuild" -exec sed -i "s/$pattern/$line\n$pattern/g" {} \;
