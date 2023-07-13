def2a3b4a0a3cb57a3c44dd5c842a9cf146c0e15
Anagnostakis Ioannis <elahistos@yahoo.gr>
2023-07-13 21:25:01 +0300

    update

--- desktop/Tela-icon-theme/doinst.sh
+++ desktop/Tela-icon-theme/doinst.sh
@@ -1,13 +1,7 @@
-if [ -x /usr/bin/update-desktop-database ]; then
-  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
-fi
-
-if [ -x /usr/bin/update-mime-database ]; then
-  /usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
-fi
-
-if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
-  if [ -x /usr/bin/gtk-update-icon-cache ]; then
-    /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
+for theme_dir in usr/share/icons/Tela usr/share/icons/Tela-dark; do
+  if [ -e $theme_dir/icon-theme.cache ]; then
+    if [ -x /usr/bin/gtk-update-icon-cache ]; then
+      /usr/bin/gtk-update-icon-cache -f $theme_dir >/dev/null 2>&1
+    fi
   fi
-fi
+done
